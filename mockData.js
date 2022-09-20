e2a09a9400c21550fab837a2277e158297eede89
Franziska S <fra.se@posteo.net>
2022-09-20 12:21:01 +0200

    created mockData File

---
+++ mockData.js
@@ -0,0 +1,2 @@
+export const mockData =
+  "Speaker, Topic, Date, Words \nAlexander Abel, Education Policy, 2012-10-30, 5310 \nBernhard Belling, Coal Subsidies, 2012-11-05, 1210 \nCaesare Collins, Coal Subsidies, 2012-11-06, 1119 \nAlexander Abel, Internal Security, 2012-12-11, 911";
