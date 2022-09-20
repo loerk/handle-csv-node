2b8f80bf799f4fccf62a1f06e4365c7d4814ee44
Franziska S <fra.se@posteo.net>
2022-09-20 12:27:53 +0200

    added controller

---
+++ controller/evaluationController.js
@@ -0,0 +1,25 @@
+import * as fs from "fs";
+import csv from "csvtojson";
+
+import { mockData } from "../mockData.js";
+import { leastWords, mostSecurity, mostSpeaches } from "../helpers/_helpers.js";
+export const getEvaluatedData = async (req, res) => {
+  const { url } = req.query;
+  if (!url)
+    return res
+      .status(403)
+      .json({ message: "please enter at least one url to the request" });
+  // creates MockData CSV from given string
+  fs.writeFileSync("FILE.CSV", mockData);
+  const result = { mostSpeaches: null, mostSecurity: null, leastWordy: null };
+
+  const politicalSpeakers = await csv().fromFile("FILE.CSV");
+  const leastWordy = leastWords(politicalSpeakers);
+  const mostSec = mostSecurity(politicalSpeakers);
+  const mostSpeachesIn2013 = mostSpeaches(politicalSpeakers);
+  (result.mostSpeaches = mostSpeachesIn2013),
+    (result.mostSecurity = mostSec),
+    (result.leastWordy = leastWordy);
+
+  res.status(200).json(result);
+};
